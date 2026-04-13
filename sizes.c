42f239595c7c236ac4211021fb64a44cd844dfa8
Agustin Azcui <agustinazcui@gmail.com>
2026-04-13 18:06:11 -0400

    sizes

---
+++ sizes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("%d", sizeof(char));
+    printf("%d", sizeof(float));
+    printf("%d", sizeof(short));
+    printf("%d", sizeof(unsigned int));
+    printf("%d", sizeof(int));
+    printf("%d", sizeof(char));
+}
